619ee868c14d8b37bf0bec69bf1d958cbf94b793
nikhil0235 <rajnikhil705@gmail.com>
2024-01-06 15:45:38 +0530

    g

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://turbotracker.vercel.app/"],
     credentials: true,
   })
 );
