d4c7cb0d7dcb657ed7b28fab1c6cebaa315b0e72
nikhil0235 <rajnikhil705@gmail.com>
2024-01-06 15:37:41 +0530

    d

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
   })
 );
